8a24dc2f23fd4c523bdb6d9d84a3383b56372853
Blackharddd <djalexmurcer@gmail.com>
2022-05-25 16:23:59 +0300

    -added display_in_booking meta condition checking;

--- booking.php
+++ booking.php
@@ -137,9 +137,11 @@ $services = pdp_get_pricelist( $main_salon );
                                             <div class="form-row form-row--one">
                                                 <div class="form-col">
                                                     <select name="salon" data-icon="location" data-confirm="<?=__( 'Смена салона удалит все услуги из корзины. Вы уверенны?', 'pdp' ); ?>">
-	                                                    <?php foreach( $salons as $key => $salon ) : ?>
-                                                            <option value="<?=$salon['id']; ?>" <?=isset( $_GET['salonId'] ) && intval( $_GET['salonId'] ) === $salon['id'] ? 'selected' : ( !isset( $_GET['salonId'] ) && $salon['id'] === $main_salon ? 'selected' : '' ); ?>><?="{$salon['city']}, {$salon['name']}"; ?></option>
-	                                                    <?php endforeach; ?>
+                                                        <?php foreach( $salons as $key => $salon ) : ?>
+                                                            <?php if( carbon_get_post_meta( $salon['id'], 'display_in_booking' ) === 'yes' ) : ?>
+                                                                <option value="<?=$salon['id']; ?>" <?=isset( $_GET['salonId'] ) && intval( $_GET['salonId'] ) === $salon['id'] ? 'selected' : ( !isset( $_GET['salonId'] ) && $salon['id'] === $main_salon ? 'selected' : '' ); ?>><?="{$salon['city']}, {$salon['name']}"; ?></option>
+                                                            <?php endif; ?>
+                                                        <?php endforeach; ?>
                                                     </select>
                                                 </div>
                                             </div>
