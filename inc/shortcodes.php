9dcc0b554a04f69406e01d6f898806e06d7dbe42
Blackharddd <djalexmurcer@gmail.com>
2022-06-22 23:16:59 +0300

    -fixed minor bug;

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -9,7 +9,7 @@ function pdp_city_salons_shortcode( $atts ){
 		'city' => ''
 	), $atts, 'city_salons' );
 
-	$city_term = get_term( pll_get_term( $atts['city'] ) );
+	$city_term = get_term( pll_get_term( $atts['city'] ) ? pll_get_term( $atts['city'] ) : $atts['city'] );
 	$salons = pdp_get_salons_data( $city_term->name );
 
 	ob_start();
